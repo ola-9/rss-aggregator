b0b2af348f7f71be059bf18b4ea8ffe0525ac6e6
Olga Kova <olgavpj@gmail.com>
2022-01-13 11:20:41 +0300

    add develop & build to Makefile

--- Makefile
+++ Makefile
@@ -6,3 +6,10 @@ lint:
 
 f-lint:
 	npx eslint --fix .
+
+develop:
+	npx webpack serve
+
+build:
+	rm -rf dist
+	NODE_ENV=production npx webpack
--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const test = () => {
-  // const element = document.createElement('div');
-  // Lodash, currently included via a script, is required for this line to work
-  // element.innerHTML = 'Hello world!11';
-  // return element;
-  console.log('still working...!');
-};
-
-// document.body.appendChild(test());
+// const test = () => {
+//   console.log('test!');
+// };
