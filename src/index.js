3a874f38a89ae9c8149c8b52a374c5302144b094
Olga Kova <olgavpj@gmail.com>
2022-01-21 14:10:55 +0300

    fix 7

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
-import 'bootstrap';
-// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// import 'bootstrap';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import i18next from 'i18next';
-import ru from './locales/ru';
+// import i18next from 'i18next';
+// import ru from './locales/ru';
 import app from './application.js';
 
-const runApp = () => {
-  const i18nextIntance = i18next.createInstance();
-  i18nextIntance.init({
-    lng: 'ru',
-    debug: true,
-    resources: {
-      ru,
-    },
-  }).then(() => app(i18nextIntance));
-};
+// const runApp = () => {
+//   const i18nextIntance = i18next.createInstance();
+//   i18nextIntance.init({
+//     lng: 'ru',
+//     debug: true,
+//     resources: {
+//       ru,
+//     },
+//   }).then(() => app(i18nextIntance));
+// };
 
-runApp();
-
+export default () => app();
 // export default runApp;
