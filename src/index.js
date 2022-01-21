55ac2357ce9c3f9c58e9c9536c8917e646ac23fa
Olga Kova <olgavpj@gmail.com>
2022-01-21 14:28:04 +0300

    fix add button

--- src/index.js
+++ src/index.js
@@ -1,20 +1,7 @@
 // import 'bootstrap';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import i18next from 'i18next';
-// import ru from './locales/ru';
 import app from './application.js';
 
-// const runApp = () => {
-//   const i18nextIntance = i18next.createInstance();
-//   i18nextIntance.init({
-//     lng: 'ru',
-//     debug: true,
-//     resources: {
-//       ru,
-//     },
-//   }).then(() => app(i18nextIntance));
-// };
-
+// app();
 export default () => app();
-// export default runApp;
