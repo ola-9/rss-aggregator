d1bfa8842f1732a388afef03177563d9f03d8e56
Olga Kova <olgavpj@gmail.com>
2022-01-28 23:09:09 +0300

    amend trackUpdate to be async

--- src/update.js
+++ src/update.js
@@ -5,30 +5,29 @@ import getProxyUrl from './util';
 import parseData from './parser';
 
 const trackUpdates = (state, watchedState) => {
-  state.data.urls.forEach((url) => {
-    axios.get(getProxyUrl(url))
-      .then((response) => {
-        const [{ id }] = state.data.feeds.filter((feed) => feed.url === url);
-        const { posts } = parseData(response);
-        const postsToRender = posts
-          .filter(({ postTitle: title1 }) => !state.data.posts
-            .some(({ postTitle: title2 }) => title2 === title1));
-        postsToRender.forEach((post) => {
-          post.id = _.uniqueId('post_');
-          post.feedId = id;
-        });
-        state.update.postsToRender = postsToRender;
-        state.data.posts = state.data.posts.concat(postsToRender);
-        watchedState.update.updateState = 'updated';
-        state.update.updateState = null;
-        state.update.postsToRender = null;
-      })
-      .catch((error) => {
-        console.log('Network error>>>>', error);
+  const promises = state.data.urls.map((url) => axios.get(getProxyUrl(url))
+    .then((response) => {
+      const [{ id }] = state.data.feeds.filter((feed) => feed.url === url);
+      const { posts } = parseData(response);
+      const postsToRender = posts
+        .filter(({ postTitle: title1 }) => !state.data.posts
+          .some(({ postTitle: title2 }) => title2 === title1));
+      postsToRender.forEach((post) => {
+        post.id = _.uniqueId('post_');
+        post.feedId = id;
       });
-  });
+      state.update.postsToRender = postsToRender;
+      state.data.posts = state.data.posts.concat(postsToRender);
+      watchedState.update.updateState = 'updated';
+      state.update.updateState = null;
+      state.update.postsToRender = null;
+    })
+    .catch((error) => {
+      console.log('Network error>>>>', error);
+    }));
 
-  setTimeout(() => trackUpdates(state, watchedState), 5000);
+  Promise.all(promises)
+    .then(() => setTimeout(() => trackUpdates(state, watchedState), 5000));
 };
 
 export default trackUpdates;
